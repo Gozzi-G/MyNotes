8ac51647c93ba06c1f8c420c3c999b3dc625fca9
Gozzy-G <struck.yury@gmail.com>
2021-01-07 17:46:45 +0300

    add Note class

---
+++ app/src/main/java/com/example/mynotes/Note.java
@@ -0,0 +1,33 @@
+package com.example.mynotes;
+
+import android.content.SharedPreferences;
+
+public class Note {
+    private String title;
+    private String description;
+    private String dayOfWeak;
+    private int priority;
+
+    public Note (String title, String description, String dayOfWeak, int priority) {
+        this.title = title;
+        this.description = description;
+        this.dayOfWeak = dayOfWeak;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return getDescription();
+    }
+
+    public String getDayOfWeak() {
+        return dayOfWeak;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
